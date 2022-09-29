e20fe3160a66ead54472229337ea40a9f692bb6f
FilipVidlak <72192554+FilipVidlak@users.noreply.github.com>
2022-09-29 11:32:36 +0200

    Add files via upload

---
+++ test_ukol910_binarnisoubory/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace test_ukol910_binarnisoubory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aktuální soubor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Opravený soubor";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(598, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Úkol 10";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(598, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 62);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Zobrazit a upravit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(93, 150);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 244);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(344, 150);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 244);
+            this.listBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Úkol 9 ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ test_ukol910_binarnisoubory/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace test_ukol910_binarnisoubory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Random random = new Random();
+                FileStream aktualnisoubor = new FileStream(@"..\..\cisla.dat", FileMode.Create, FileAccess.Write);
+                BinaryWriter psat = new BinaryWriter(aktualnisoubor);
+                for (int i = 0; i < 10; i++)
+                {
+                    int cislo = random.Next(-10, 11);
+                    psat.Write(cislo.ToString());
+                }
+                aktualnisoubor.Close();
+                psat.Close();
+               
+            }
+            catch
+            {
+                MessageBox.Show("Něco se nepovedlo!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileStream aktualnisoubor2 = new FileStream(@"..\..\cisla.dat", FileMode.Open, FileAccess.Read);
+            BinaryReader precist = new BinaryReader(aktualnisoubor2);
+            string cisla = string.Empty;
+            precist.BaseStream.Position = 0;
+            while (precist.BaseStream.Position < precist.BaseStream.Length)
+            {
+                cisla = precist.ReadString();
+                listBox1.Items.Add(precist.ReadString());
+            }
+            aktualnisoubor2.Close();
+            precist.Close();
+            FileStream upravenysoubor = new FileStream(@"..\..\cisla.dat", FileMode.Create, FileAccess.Write);
+            BinaryWriter prepis = new BinaryWriter(upravenysoubor);
+            char posledniznak = cisla[cisla.Length - 1];
+            int lol = 1;
+            if (posledniznak % 2 == 1)
+            {
+                for (int i = 0; i < cisla.Length; i++)
+                {
+
+                    if (cisla[i] % 2 == 0)
+                    {
+                        cisla[i]++;
+                    }
+                    listBox2.Items.Add(cisla);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < cisla.Length; i++)
+                {
+                    if (cisla[i] % 2 == 1)
+                    {
+                        cisla[i] *= 2; //nelze ve stringu prirazovat konkretni hodnoty a u toho jsem koncil, nemel jsem cas to prepisovat..
+                    }
+                    listBox2.Items.Add(cisla);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
